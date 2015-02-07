b2d1fabf8a0ee8452e1216b5316c27c4a3bb6d85
ramshresh <sendmail4ram@gmail.com>
2015-02-08 02:33:01 +0545

    setup correct config for production and development environment. Moved $config['components']['db'] and $config['components']['mailer'] to common\config\main-local.php from common\config\main.php

--- common/config/main.php
+++ common/config/main.php
@@ -2,12 +2,28 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'user' => [
+            //'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'class' => 'common\modules\user\components\User',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'assetManager' => [
             'linkAssets' => true,
         ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'common\modules\user\Module',
+        ],
+        'reporting' => [
+            'class' => 'common\modules\reporting\Module',
+        ],
+        'gridview' => [
+            'class' => '\kartik\grid\Module'
+        ]
     ],
 
 ];
