32920fa0fee7750ce8acd37e303610b8ed043ecb
ramshresh <sendmail4ram@gmail.com>
2015-02-16 10:17:57 +0545

    add file

---
+++ common/modules/reporting/models/search/Damage.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace common\modules\reporting\models\search;
+
+use common\modules\reporting\models\ReportItem;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\modules\reporting\models\Damage as DamageModel;
+
+/**
+ * Damage represents the model behind the search form about `common\modules\reporting\models\Damage`.
+ */
+class Damage extends DamageModel
+{
+    //{{{ initialization
+    public function init()
+    {
+        parent::init();
+        /**
+         * 'search' scenario has been defined in model ReportItem
+         *  Models 'DamageSearch' --extends--> 'DamageModel' --extends--> 'ReportItemModel'
+         */
+        $this->scenario = 'search';
+    }
+    //}}} ./Initialization
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'reportitem_id', 'quantity', 'status'], 'integer'],
+            [['units_shortname', 'units_displayname'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = DamageModel::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        //{{{ dataprovider's sort attributes for ReportItem
+        /**
+         * Provides Sorting function (OR-links A->Z and Z->A) in GridView
+         * @IMPORTANT These assignments must be made before executing:
+         *          $this->load($params);
+         */
+        $dataProvider->sort->attributes['item_name']=[
+            'asc' => [ReportItem::tableName() . '.item_name' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.item_name' => SORT_DESC],
+            'label' => 'Item Name',
+        ];
+        $dataProvider->sort->attributes['type']=[
+            'asc' => [ReportItem::tableName() . '.type' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.type' => SORT_DESC],
+            'label' => 'Type',
+        ];
+        $dataProvider->sort->attributes['is_verified']=[
+            'asc' => [ReportItem::tableName() . '.is_verified' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.is_verified' => SORT_DESC],
+            'label' => 'Is Verified',
+        ];
+        $dataProvider->sort->attributes['timestamp_created']=[
+            'asc' => [ReportItem::tableName() . '.timestamp_created' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.timestamp_created' => SORT_DESC],
+            'label' => 'Created at',
+        ];
+        $dataProvider->sort->attributes['timestamp_updated']=[
+            'asc' => [ReportItem::tableName() . '.timestamp_updated' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.timestamp_updated' => SORT_DESC],
+            'label' => 'Updated at',
+        ];
+        $dataProvider->sort->attributes['subtype_name']=[
+            'asc' => [ReportItem::tableName() . '.subtype_name' => SORT_ASC],
+            'desc' => [ReportItem::tableName() . '.subtype_name' => SORT_DESC],
+            'label' => 'Subtype',
+        ];
+        //}}} ./dataprovider's sort attributes for ReportItem
+
+
+
+        if (!($this->load($params) && $this->validate())) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+
+            // Preloading related <reportitem> model with join
+            $query->joinWith(['reportitem']);
+
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'reportitem_id' => $this->reportitem_id,
+            'quantity' => $this->quantity,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'units_shortname', $this->units_shortname])
+            ->andFilterWhere(['like', 'units_displayname', $this->units_displayname]);
+
+        $query->joinWith(
+
+            [/**
+             * For making Columns from relationship <reportitem> searchable
+             */
+                'reportitem' => function ($q) {
+                /**
+                 * sample
+                 * $q->andFilterWhere(['like', ReportItem::tableName().".item_name", $this->itemName]);
+                 * ---OR---Equivalent
+                 * $q->where(ReportItem::tableName() . ".item_name LIKE '%" . $this->itemName . "%' ");
+                 */
+                $q->andFilterWhere(['like', ReportItem::tableName().".item_name", $this->item_name]);
+                $q->andFilterWhere(['like', ReportItem::tableName().".subtype_name", $this->subtype_name]);
+                $q->andFilterWhere(['=', ReportItem::tableName().".type", $this->type]);
+                $q->andFilterWhere(['=', ReportItem::tableName().".is_verified", $this->is_verified]);
+                $q->andFilterWhere(['=', ReportItem::tableName().".is_verified", $this->timestamp_created]);
+                $q->andFilterWhere(['=', ReportItem::tableName().".is_verified", $this->timestamp_created]);
+            }]
+        );
+
+        return $dataProvider;
+    }
+
+}
---
+++ common/modules/reporting/models/search/Incident.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace common\modules\reporting\models\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\modules\reporting\models\Incident as IncidentModel;
+
+/**
+ * Incident represents the model behind the search form about `common\modules\reporting\models\Incident`.
+ */
+class Incident extends IncidentModel
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'reportitem_id', 'status'], 'integer'],
+            [['timestamp_occurance', 'duration'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = IncidentModel::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'reportitem_id' => $this->reportitem_id,
+            'timestamp_occurance' => $this->timestamp_occurance,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'duration', $this->duration]);
+
+        return $dataProvider;
+    }
+}
