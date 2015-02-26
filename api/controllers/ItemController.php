c569dd8a1ace0a9993aa5f11bd5634e55868fd9e
ramshresh <sendmail4ram@gmail.com>
2015-02-26 11:29:19 +0545

    bulk commit

--- api/controllers/ItemController.php
+++ api/controllers/ItemController.php
@@ -13,26 +13,5 @@ class ItemController extends \yii\rest\ActiveController
 {
     public $modelClass = 'api\common\models\Item';
 
-    public function behaviors()
-    {
-        return [
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::className(),
-                'cors' => [
-                    // restrict access to
-                    'Origin' => ['*'],
-                    'Access-Control-Request-Method' => ['GET'],
-                    // Allow only POST and PUT methods
-                    'Access-Control-Request-Headers' => ['X-Wsse'],
-                    // Allow only headers 'X-Wsse'
-                    'Access-Control-Allow-Credentials' => true,
-                    // Allow OPTIONS caching
-                    'Access-Control-Max-Age' => 3600,
-                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
-                ],
 
-            ],
-        ];
-    }
 }
