499d3fb2fd6b13b7c7248729606a95314940abd2
ramshresh <sendmail4ram@gmail.com>
2015-03-27 12:15:42 +0545

    set alias of Custom directories cgi-bin and uploads

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,10 +1,14 @@
 <?php
+
 Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
 
+// Custom directories
+Yii::setAlias('cgi-bin', dirname(__DIR__));
+Yii::setAlias('uploads', dirname(__DIR__));
 
 //----------------------------------------------Sync Assets---------------------------------------------
 //---Enable to force copy the asset files---//
