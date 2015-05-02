43ebbc79850efbb6d17880258e5ca8edfcfecb70
ramshresh <sendmail4ram@gmail.com>
2015-05-02 11:05:58 +0545

    little mod

--- console/migrations/m150502_031859_module_rapid_assessment.php
+++ console/migrations/m150502_031859_module_rapid_assessment.php
@@ -5,26 +5,20 @@ use yii\db\Migration;
 
 class m150502_031859_module_rapid_assessment extends Migration
 {
+
     public function safeUp()
     {
         Yii::$app->db->createCommand('CREATE SCHEMA IF NOT EXISTS "rapid_assessment"')->execute();
         Yii::$app->db->createCommand('CREATE EXTENSION IF NOT EXISTS postgis;')->execute();
         Yii::$app->db->createCommand('CREATE EXTENSION IF NOT EXISTS hstore;')->execute();
         $this->createTables();
+        //$this->populateTables();
     }
+
 
     public function safeDown()
     {
         Yii::$app->db->createCommand('DROP SCHEMA IF EXISTS "rapid_assessment" CASCADE')->execute();
-             /*echo "m150502_005701_rapid_assessment_create_schema cannot be reverted.\n";
-        return false;*/
         return true;
-    }
-
-    public function createTables(){
-        $sql ='';
-        Yii::$app->db->createCommand($sql)->execute();
-
-
     }
 }
