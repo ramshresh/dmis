8c7046c4ba25a76f3e01eb4e8a1df1e816b54137
ramshresh <sendmail4ram@gmail.com>
2015-05-02 10:32:57 +0545

    little mod

--- console/migrations/m150502_031859_module_rapid_assessment.php
+++ console/migrations/m150502_031859_module_rapid_assessment.php
@@ -21,30 +21,7 @@ class m150502_031859_module_rapid_assessment extends Migration
     }
 
     public function createTables(){
-        $sql = <<<SQL
-
-            CREATE TABLE "rapid_assessment".item
-            (
-              id bigserial NOT null,
-              name character varying(255) NOT NULL,
-              display_name character varying(255) DEFAULT NULL::character varying,
-              tags character varying(255)[] DEFAULT NULL::character varying[],
-              meta_hstore hstore,
-              meta_json json,
-              is_verified boolean DEFAULT false,
-              CONSTRAINT pk_item_id PRIMARY KEY (id),
-              CONSTRAINT unique_item_display_name UNIQUE (display_name),
-              CONSTRAINT unique_item_name UNIQUE (name)
-            )
-            WITH (
-              OIDS=FALSE
-            );
-            ALTER TABLE "rapid_assessment".item
-              OWNER TO postgres;
-
-
-
-SQL;
+        $sql ='';
         Yii::$app->db->createCommand($sql)->execute();
 
 
