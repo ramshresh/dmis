636c9749bc6e2c1cc20330e1a9643b206e208c6c
ramshresh <sendmail4ram@gmail.com>
2015-05-03 11:32:22 +0545

    removed file writing

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -701,7 +701,7 @@ var clickHandlerReportItem = function (evt) {
             jsonpCallback: 'parseResponse'
         }).then(function (response) {
 
-        console.log(response.features[0].id.split('.'));
+        console.log(response.features[0].id.split('.')[1]);
             result = parser.readFeatures(response);
             if (result.length) {
                 var popupContent = '';
