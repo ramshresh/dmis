61de0f18577b31803c814e5c3b397def59c0cfd4
ramshresh <sendmail4ram@gmail.com>
2015-05-05 13:38:11 +0545

    runtime ignore

---
+++ lab/spatial_filter/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 5/1/2015
+ * Time: 5:02 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Disaster Profile</title>
+    <link rel="stylesheet" href="../lib/jquery-ui-1-11-2-custom/jquery-ui.min.css">
+    <link rel="stylesheet" href="../lib/bootstrap/3/2/0/dist/css/bootstrap.min.css">
+    <script src="../lib/jquery.min.js"></script>
+    <script src="../lib/jquery-ui-1-11-2-custom/jquery-ui.min.js"></script>
+    <script src="../lib/bootstrap/3/2/0/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<select id="extent_type">
+    <option value="district">District</option>
+    <option value="vdc">Vdc</option>
+</select>
+
+<script>
+
+</script>
+</body>
+</html>
