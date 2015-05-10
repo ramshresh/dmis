55ce820a9926822019e9faeeee3783a12c84702a
ramshresh <sendmail4ram@gmail.com>
2015-05-10 12:46:00 +0545

    a

---
+++ frontend/modules/rapid_assessment/controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\modules\rapid_assessment\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
