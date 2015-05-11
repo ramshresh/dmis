b37f3c09f9166ef30cfb70f92fbe82e199f632d3
ramshresh <sendmail4ram@gmail.com>
2015-05-12 03:24:00 +0545

    popup

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -468,35 +468,26 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
             'title': 'Base',
             layers: []
         })
-        /*
-         * earthquake impact clustering start
-         * */
         var vectorSource = new ol.source.ServerVector({
-            format: new ol.format.GeoJSON({
-                //    projection: 'EPSG:3857'
-            }),
-            //   'crossOrigin':'anonymous',
-            loader: function (extent, resolution, projection) {
+            format: new ol.format.GeoJSON({}),
+            loader: function(extent, resolution, projection) {
                 var url = 'http://118.91.160.230:8080/geoserver/dmis/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=dmis:report_item&srsname=EPSG:3857&outputformat=text/javascript&format_options=callback:loadFeatures&bbox=' + extent.join(',');
-                 $.ajax({
+                $.ajax({
                     url: url,
                     type: 'GET',
                     dataType: 'jsonp',
-                    //jsonp:'jsonp',
-                    success: function (data) {
-                        //  console.log(data);
-                    },
-                    error: function (data) {
-                        //  console.log(data);
-                    },
+                    success: function(data) {},
+                    error: function(data) {},
                     timeout: 30000 // 1 minute timeout
                 });
             },
             strategy: ol.loadingstrategy.createTile(new ol.tilegrid.XYZ({
                 maxZoom: 19
             }))
-            //   projection: 'EPSG:3857'
         });
+
+
+
         var styleCache = {};
         var vector = new ol.layer.Vector({
 
@@ -505,16 +496,16 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
                 source: vectorSource
             }),
             //style: styleFunction
-            style: function (feature, resolution) {
+            style: function(feature, resolution) {
                 var size = feature.get('features').length;
-                var stroke = new ol.style.Stroke({color: 'black', width: 2});
-                var fill = new ol.style.Fill({color: 'red'});
                 if (size === 1) {
+
                     style = [new ol.style.Style({
                         image: new ol.style.Icon(({
                             src: 'http://vignette2.wikia.nocookie.net/fallout/images/7/73/Icon_damage.png/revision/latest?cb=20091010164957',
                             //  src: src_icon(),
                             offset: [1, 1],
+                            // size: [32,32],
                             scale: 0.1
                         }))
                     })]
@@ -549,27 +540,42 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
                                     offsetY: 0,
                                     rotation: 0
                                 })
-                            })];
+                            })
+                        ];
                         styleCache[size] = style;
                     }
-
-
                 }
 
                 return style;
             }
         });
         window.vector = vector;
-        var loadFeatures = function (response) {
+        var loadFeatures = function(response) {
             vectorSource.addFeatures(vectorSource.readFeatures(response));
         };
         window.loadFeatures = loadFeatures;
-        //vector.getSource().clear();
         overlayGroup.getLayers().push(vector);
         /*end*/
+
+        var nepal_vdc = new ol.layer.Tile({
+            title: 'Nepal VDC',
+            name: 'Nepal VDC',
+            type: 'overlay',
+            source: new ol.source.TileWMS(
+                ({
+                    url: 'http://118.91.160.230:8080/geoserver/wms',
+                    params: {
+                        'LAYERS': 'dmis:nepal_vdc',
+                        'TILED': true
+                    }
+                }))
+        });
+
+        //overlayGroup.getLayers().push(nepal_vdc);
+
         var view = new ol.View({
 
-            center: ol.proj.transform([87, 29], 'EPSG:4326', 'EPSG:3857'),
+            center: ol.proj.transform([87.2345, 29.3535], 'EPSG:4326', 'EPSG:3857'),
             zoom: 2
         });
         var osm = new ol.layer.Tile({
@@ -581,8 +587,12 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
         var key = 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3';
 
         var imagery = new ol.layer.Tile({
-            source: new ol.source.BingMaps({key: key, imagerySet: 'Aerial'})
+            source: new ol.source.BingMaps({
+                key: key,
+                imagerySet: 'Aerial'
+            })
         });
+
         baseGroup.getLayers().push(osm);
         baseGroup.getLayers().push(imagery);
 
@@ -595,23 +605,75 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
                 baseGroup,
                 overlayGroup
             ],
-            controls: ol.control.defaults({
-                attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-                    collapsible: false
-                })
-            }).extend([
-                new ol.control.LayerSwitcher(),
-                new ol.control.ZoomToExtent({
+            controls:
+            //ol.control.defaults({
+            //    attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+            //        collapsible: false
+            //    })
+            // }).extend(
+                [
+                    new ol.control.LayerSwitcher(),
+                    new ol.control.Zoom(),
+                    // new ol.control.ZoomToExtent({
                     //extent: [-180,-90,180,90]
-                    extent: [8858052.801082317, 2602714.8048996064, 10081045.253645137, 3825707.2574624266]
-                })
-            ]),
+                    //    extent: [8858052.801082317, 2602714.8048996064, 10081045.253645137, 3825707.2574624266]
+                    // })
+                ],
+            //),
 
             view: view
         });
+
+        var radius = 100;
+        $(document).keydown(function(evt) {
+            if (evt.which === 38) {
+                radius = Math.min(radius + 5, 150);
+                map.render();
+            } else if (evt.which === 40) {
+                radius = Math.max(radius - 5, 25);
+                map.render();
+            }
+        });
+
+        // get the pixel position with every move
+        var mousePosition = null;
+        $(map.getViewport()).on('mousemove', function(evt) {
+            mousePosition = map.getEventPixel(evt.originalEvent);
+            map.render();
+        }).on('mouseout', function() {
+            mousePosition = null;
+            map.render();
+        });
+
+        // before rendering the layer, do some clipping
+        imagery.on('precompose', function(event) {
+            var ctx = event.context;
+            var pixelRatio = event.frameState.pixelRatio;
+            ctx.save();
+            ctx.beginPath();
+            if (mousePosition) {
+                // only show a circle around the mouse
+                ctx.arc(mousePosition[0] * pixelRatio, mousePosition[1] * pixelRatio,
+                    radius * pixelRatio, 0, 2 * Math.PI);
+                ctx.lineWidth = 5 * pixelRatio;
+                ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+                ctx.stroke();
+            }
+            ctx.clip();
+        });
+
+        // after rendering the layer, restore the canvas context
+        imagery.on('postcompose', function(event) {
+            var ctx = event.context;
+            ctx.restore();
+        });
+
+
         /***********function for counting unique values in an array**********/
         function unique_count(arr) {
-            var a = [], b = [], prev;
+            var a = [],
+                b = [],
+                prev;
 
             arr.sort();
             for (var i = 0; i < arr.length; i++) {
@@ -626,75 +688,29 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
 
             return [a, b];
         }
-        var highlightStyleCache = {};
+        /*********************/
 
-        var featureOverlay = new ol.FeatureOverlay({
-            map: map,
-            style: function (feature, resolution) {
-                //console.log(feature);
-                //console.log(resolution);
-                // var text = resolution < 5000 ? feature.get('name') : '';
-                var size = feature.get('features').length;
-                if (size == 1) {
-                    var text = resolution < 5000 ? (feature.values_.features[0].values_.item_name) : '';
-                }
-                else {
-                    var text = '';
-                    var text_array = [];
-                    $.each(feature.values_.features, function (index, value) {
-                        if (value.values_.type === "need") {
-                            text_array.push(value.values_.item_name);
-                        }
-                    })
-                    //console.log($.unique(text_array));
-                    var result = unique_count(text_array);
-                    //console.log('[' + result[0] + '][' + result[1] + ']')
-                }
-
-                if (!highlightStyleCache[text]) {
-                    highlightStyleCache[text] = [new ol.style.Style({
-                        stroke: new ol.style.Stroke({
-                            color: '#f00',
-                            width: 1
-                        }),
-                        fill: new ol.style.Fill({
-                            color: 'rgba(0,150,0,0.1)'
-                        }),
-                        text: new ol.style.Text({
-                            font: '12px Calibri,sans-serif',
-                            text: text,
-                            fill: new ol.style.Fill({
-                                color: '#000'
-                            }),
-                            stroke: new ol.style.Stroke({
-                                color: '#00f',
-                                width: 3
-                            })
-                        })
-                    })];
-                }
-                return highlightStyleCache[text];
-            }
-        });
 
         var popup = new ol.Overlay.Popup();
         map.addOverlay(popup);
-        var img_src;
-        var image = function (id) {
+        var image = function(id) {
             $.ajax({
-                url: '/girc/dmis/api/rapid_assessment/report-items',
+                url: 'http://118.91.160.230/girc/dmis/api/rapid_assessment/report-items',
                 data: {
                     expand: 'galleryImages',
                     id: id
                 },
-                success: function (data) {
+                success: function(data) {
                     var src;
                     if (data) {
+                        console.log(data);
                         if (data[0]) {
                             if (data[0].galleryImages[0]) {
                                 if (data[0].galleryImages[0].src) {
                                     src = data[0].galleryImages[0].src;
+                                    console.log("src");
                                     console.log(src);
+                                    console.log("src");
                                 }
                             }
                         }
@@ -703,95 +719,116 @@ $this->registerJs($JsAddressSearch, $this::POS_READY);
                     }
 
                     if (src) {
-                        img_src = '<img src="' + src + '" alt="" style="height:auto;width:200px;">';
-                    }
-                    else {
-                        img_src = '';
+                        img_src = '<img src="http://118.91.160.230' + src + '" alt="" style="height:auto;width:200px;">';
+                        return img_src;
                     }
                 }
             });
-            console.log(img_src);
-            return img_src;
+
         }
         var highlight;
-        var ids_array;
-        var displayFeatureInfo = function (pixel) {
+        var displayFeatureInfo = function(pixel) {
 
-            var feature = map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+            var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
                 return feature;
             });
 
             var info = document.getElementById('info');
-            size = feature.get('features').length;
+            try {
+                var size = feature.get('features').length;
+            } catch (e) {}
+            //  console.log(size);
             if (feature) {
                 coor_feature = feature.values_.geometry.flatCoordinates;
                 if (size == 1) {
                     console.log(feature);
-                    //fid = feature.values_.features[0].id_;
+                    fid = feature.values_.features[0].id_;
                     //id = fid.split('-')[1];
-                    id = feature.values_.features[0].id;
-                    console.log("id");
-                    console.log(id);
-                    console.log("id");
-                    //	info.innerHTML = feature.getId() + ': ' + feature.get('name');
-                    popup.show(coor_feature, '<h4>' + feature.values_.features[0].values_.item_name + '</h4>Human Casulty : ' + feature.values_.features[0].values_.magnitude + image(id));
-                }
-                else {
+                    id = fid.split('.')[1];
+                    // popup.show(coor_feature, '<h4>' + feature.values_.features[0].values_.item_name + '</h4><p>Human Casulty :</p> ' + feature.values_.features[0].values_.magnitude + image(id));
+                    popup.show(
+                        coor_feature,
+                        '<h4>' + feature.values_.features[0].values_.item_name
+                    + '<hr>' +
+                    image(id));
+                    popup.
+                    // popup.show(coor_feature, '<h4>' + feature.values_.features[0].values_.item_name);
+                } else {
 
                     var text = '';
-                    var text_array = [];
-                    var ids_array = [];
-                    var user_id_array=[]
-                    $.each(feature.values_.features, function (index, value) {
-                        ids_array.push(value.id_);
-                        user_id_array.push(value.values_.user_id);
-                        text_array.push(value.values_.item_name);
+                    var items_array_incident = [];
+                    var items_array_need = [];
+                    var items_array_impact = [];
+                    var items_array_supplied = [];
+
+                    var mag_array_incident = [];
+                    var mag_array_need = [];
+                    var mag_array_impact = [];
+                    var mag_array_supplied = [];
+
+                    $.each(feature.values_.features, function(index, value) {
+                        if (value.values_.type == "incident") {
+                            items_array_incident.push(value.values_.class_name);
+                        } else if (value.values_.type == "impact") {
+                            items_array_impact.push(value.values_.item_name);
+                            mag_array_impact.push(value.values_.magnitude);
+                        } else {
+                            items_array_need.push(value.values_.item_name);
+                            mag_array_need.push(value.values_.magnitude);
+                        }
                     });
 
 
-                    var result = unique_count(text_array);
 
-                    if (result[0].length == result[1].length) {
-                        length = result[0].length;
+                    var mapping_impact = {};
+                    var mapping_need = {};
 
-                        list = [];
-                        for (var i = 0; i < length; i++) {
-                            row = {};
-                            row.value = result[0][i];
-                            row.count = result[1][i];
-                            list.push(row);
+                    var incidents = unique_count(items_array_incident);
+                    var incident_unique = incidents[0];
+                    var incident_counts = incidents[1];
+
+                    var popup_content_incident = '<strong>Report Details</strong><hr><strong>Incident</strong><table class="table">';
+                    for (i = 0; i < incidents.length + 1; i++) {
+                        if( typeof incident_unique[i]==="undefined"){
+
                         }
-                        var popup_content = '<h4>Report Detail</h4><hr>';
-                        $.each(list, function (index, value) {
-                            popup_content += value.value + ' : ' + value.count + '<br>';
-                        });
-                            popup_content+='<hr> <a id = "popupimages" >view images</a>';
-
-
-                        popup.show(coor_feature, popup_content);
-
-                        $('a#popupimages').click(function(evet){
-
-                            showPopupImages(ids_array);
-                        });
-
-                    } else {
-                        alert('error arrays not equal');
+                        else{
+                            popup_content_incident += '<tr><td>' + incident_counts[i] + '</td><td>house(s)</td><td>' + incident_unique[i] + '</td></tr>';
+                        }
                     }
+
+                    //impact
+                    for (var i = 0, count = items_array_impact.length; i < count; i++) {
+                        mapping_impact[items_array_impact[i]] = (mapping_impact[items_array_impact[i]] || 0) + mag_array_impact[i];
+                    }
+                    var popup_content_impact = '</table><hr><strong>Impact</strong><table class="table">';
+
+                    for (var item in mapping_impact) {
+                        popup_content_impact += '<tr><td>' + item + '</td><td>:' + mapping_impact[item] + '</td></tr>';
+                        //count_items_people = item + ': ' + mapping[item];
+                    }
+
+                    //need
+                    for (var i = 0, count = items_array_need.length; i < count; i++) {
+                        mapping_need[items_array_need[i]] = (mapping_need[items_array_need[i]] || 0) + mag_array_need[i];
+                    }
+                    var popup_content_need = '</table><hr><strong>Need<strong><table class="table">';
+
+                    for (var item in mapping_need) {
+                        popup_content_need += '<tr><td>' + item + '</td><td>for ' + mapping_need[item] + ' people </td></tr>';
+                        //count_items_people = item + ': ' + mapping[item];
+                    }
+
+                    popup_content_need +='</table>';
+                    popup.show(coor_feature, popup_content_incident + popup_content_impact + popup_content_need);
 
                 }
             } else {
+                $(".ol-popup").hide();
             }
-
         };
 
-        function showPopupImages(ids){
-            console.log(ids)
-        }
-
-
-        map.on('click', function (evt) {
-
+        map.on('click', function(evt) {
             displayFeatureInfo(evt.pixel);
         });
         ////////////////////////////////////////////////////////////////////////
