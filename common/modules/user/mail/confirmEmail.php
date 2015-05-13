5453e5c5c1c810f5af450e8fa353c3e4ba70c615
ramshresh <sendmail4ram@gmail.com>
2015-05-13 08:31:22 +0545

    added message

--- common/modules/user/mail/confirmEmail.php
+++ common/modules/user/mail/confirmEmail.php
@@ -14,5 +14,8 @@ use yii\helpers\Url;
 <h3><?= $subject ?></h3>
 
 <p><?= Yii::t("user", "Please confirm your email address by clicking the link below:") ?></p>
-
-<p><a href="<?= AppHelper::getAppUrlToRoute('backend',["/user/registration/confirm", "key" => $userKey->key]); ?>">Click Here to Confirm</a>
+<br>
+<p>Copy paste the following url into web browser to go to confirmation page </p>
+<em><?= AppHelper::getAppUrlToRoute('frontend',["/user/registration/confirm", "key" => $userKey->key]); ?></em>
+<br>
+<p><a href="<?= AppHelper::getAppUrlToRoute('frontend',["/user/registration/confirm", "key" => $userKey->key]); ?>">Click Here to Confirm</a>
