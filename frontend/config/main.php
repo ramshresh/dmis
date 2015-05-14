ac9c27d0b963fa748b65a1555097b31f9d39b0c8
ramshresh <sendmail4ram@gmail.com>
2015-05-14 08:30:05 +0545

    changed url

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,9 +9,8 @@ $params = array_merge(
 
 // {{{ Removing frontend/web from url @see http://www.yiiframework.com/wiki/755/how-to-hide-frontend-web-in-url-addresses-on-apache/*/
 use \yii\web\Request;
+$baseUrlBackend = str_replace('/frontend/web', '/admin', (new Request)->getBaseUrl());// also add ['vomponents']['request'] 'baseUrl' => $baseUrl,
 $baseUrl = str_replace('/frontend/web', '', (new Request)->getBaseUrl());// also add ['vomponents']['request'] 'baseUrl' => $baseUrl,
-
-
 
 //}}} ./Removing frontend/web from url
 
@@ -26,6 +25,13 @@ return [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'RViWfrrd176OveFUGaZt2MOILbofx_Bu',
         ],
+        'urlManagerBackEnd' =>[
+            'class'=>'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'baseUrl' => $baseUrlBackend,
+        ],
+
         'urlManager' => require(__DIR__ . '/components/urlManager.php'),
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
