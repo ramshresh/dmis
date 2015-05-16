7f7adf74298fe191a3f2d68b939c156537e9141d
ramshresh <sendmail4ram@gmail.com>
2015-05-16 08:32:42 +0545

    bulk change

--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,27 @@ return [
     'name'=>'DMIS',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            //'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            //'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'gircgeomatics@gmail.com',
+                'password' => 'passwordgircgeomatics',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+            'messageConfig' => [
+                'from' => ['gircgeomatics@gmail.com' => 'Admin'], // this is needed for sending emails
+                'charset' => 'UTF-8',
+            ]
+        ],
         'authClientCollection' => [
             'class' => 'yii\authclient\Collection',
             'clients' => [
