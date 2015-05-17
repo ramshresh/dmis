5ce01767241601b4e70d5d558d4425dbb1fa4ff6
ramshresh <sendmail4ram@gmail.com>
2015-05-18 01:35:08 +0545

    m

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,31 +5,8 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'pgsql:host=localhost;port=5432;dbname=dmis',
             'username' => 'postgres',
-            'password' => 'geolab345',
-
+            'password' => 'postgres',
             'charset' => 'utf8',
-        ],
-
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            //'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            //'useFileTransport' => true,
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'gircgeomatics@gmail.com',
-                'password' => 'passwordgircgeomatics',
-                'port' => '587',
-                'encryption' => 'tls',
-            ],
-            'messageConfig' => [
-                'from' => ['gircgeomatics@gmail.com' => 'Admin'], // this is needed for sending emails
-                'charset' => 'UTF-8',
-            ]
         ],
     ],
 ];
