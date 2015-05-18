399348f9da1c5dd79951f35579ccf5ffa6513093
ramshresh <sendmail4ram@gmail.com>
2015-05-18 07:33:08 +0545

    added ol3 3.5.0 min assets

--- api/modules/rapid_assessment/controllers/ReportItemController.php
+++ api/modules/rapid_assessment/controllers/ReportItemController.php
@@ -469,6 +469,7 @@ SQL;
                         case 'need':
                             $queryChildren->addSelect([
                                 'item_name',
+                                'type',
                                 'needed'=>'sum(magnitude)',
                                 'supplied'=>'sum(supplied_per_person)'
                             ]);
@@ -478,13 +479,13 @@ SQL;
                         case 'impact':
                             $queryChildren->addSelect([
                                 'item_name',
+                                'type',
                                 'count'=>'sum(magnitude)',
                             ]);
-                            $queryChildren->groupBy(['item_name']);
+                            $queryChildren->groupBy(['item_name','type']);
                             $queryChildren->having(['=', 'type', $cType]);
                             break;
                     }
-
                 }
             }
             array_push($allChildren,$queryChildren->all());
