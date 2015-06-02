fe6111a461de35e267e176a937c95a285739f5fe
ramshresh <sendmail4ram@gmail.com>
2015-06-03 05:23:15 +0545

    minor

--- frontend/controllers/DemoGeoController.php
+++ frontend/controllers/DemoGeoController.php
@@ -198,7 +198,9 @@ class DemoGeoController extends MyBaseContoller
                     $oldFolderNames[]=$file;
                     $oldFolderPath = $path.DIRECTORY_SEPARATOR.$file;
 
+                    echo '<br'.Json::encode([$file=>isset($oldNew[$file])]);
                     if(isset($oldNew[$file])){
+
                         $newFolderPath = $path.DIRECTORY_SEPARATOR.$oldNew[$file];
                         $newTempFolderPath = $tempPath.DIRECTORY_SEPARATOR.$oldNew[$file];
 
