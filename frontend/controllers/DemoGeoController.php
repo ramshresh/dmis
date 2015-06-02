7f89cdd58a34f772566e5340a5d09906584697b4
ramshresh <sendmail4ram@gmail.com>
2015-06-02 15:32:07 +0545

    minor

--- frontend/controllers/DemoGeoController.php
+++ frontend/controllers/DemoGeoController.php
@@ -130,8 +130,8 @@ class DemoGeoController extends MyBaseContoller
             $oldNew[(string)$row->column[0]]=(string)$row->column[3];
             $oldOwnerID[(string)$row->column[0]]=(string)$row->column[2];
         }
-        print_r($oldNew[0]);
-        print_r($oldOwnerID[0]);
+        print_r($oldNew[458]);
+        print_r($oldOwnerID[458]);
         Yii::$app->end();
         if(!is_dir($tempPath)){
             mkdir($tempPath);
