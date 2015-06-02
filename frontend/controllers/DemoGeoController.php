72b2fc87ff434711fdc84b3749bb410cefc6ced1
ramshresh <sendmail4ram@gmail.com>
2015-06-02 13:30:34 +0545

    minor

--- frontend/controllers/DemoGeoController.php
+++ frontend/controllers/DemoGeoController.php
@@ -101,7 +101,7 @@ class DemoGeoController extends MyBaseContoller
     public function actionMove(){
 
         $prefix = 'temp_';
-        $basePath = 'var/www/html/girc/dmis/uploads/images/building_assessment';
+        $basePath = '/var/www/html/girc/dmis/uploads/images/building_assessment';
         $path = $basePath.DIRECTORY_SEPARATOR.'gallery';
         $tempPath = $basePath.DIRECTORY_SEPARATOR.'temp';
         $pathXmlFile =$basePath.DIRECTORY_SEPARATOR.'new_old_id.xml';
