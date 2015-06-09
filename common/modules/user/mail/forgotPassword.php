c44b92fd12739be5155d1e0bebce4ead969e5772
ramshresh <sendmail4ram@gmail.com>
2015-06-09 11:17:28 +0545

    corrected reset link

--- common/modules/user/mail/forgotPassword.php
+++ common/modules/user/mail/forgotPassword.php
@@ -14,7 +14,9 @@ use yii\helpers\Url;
 
 <h3><?= $subject ?></h3>
 
+<br>
+<p>Copy paste the following url into web browser to go to password reset page </p>
+<em><?= Yii::$app->urlManagerFrontEnd->createAbsoluteUrl(["/user/registration/reset", "key" => $userKey->key]); ?></em>
+<br>Or<br>
 <p><?= Yii::t("user", "Please use this link to reset your password:") ?></p>
-
-
-<p><a href="<?= AppHelper::getAppUrlToRoute('backend',["/user/registration/reset", "key" => $userKey->key]); ?>">Click Here to Reset Password</a>
+<p><a href="<?= Yii::$app->urlManagerFrontEnd->createAbsoluteUrl(["/user/registration/reset",  "key" => $userKey->key]); ?>">Click Here to Reset Password</a>
