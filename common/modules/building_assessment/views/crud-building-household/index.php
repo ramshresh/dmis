7d1ab1287e9e455df9bcc892686ee85a94d16225
ramshresh <sendmail4ram@gmail.com>
2015-06-12 10:55:25 +0545

    minor

--- common/modules/building_assessment/views/crud-building-household/index.php
+++ common/modules/building_assessment/views/crud-building-household/index.php
@@ -1,7 +1,9 @@
 <?php
 
+use kartik\export\ExportMenu;
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
+
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\modules\building_assessment\models\search\BuildingHouseholdSearch */
@@ -19,6 +21,54 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a(Yii::t('app', 'Create Building Household'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <?php
+    $gridColumns=[
+        ['class' => 'yii\grid\SerialColumn'],
+
+        'id',
+        'owner_name',
+        'owner_contact',
+        'occupancy_type:ntext',
+        'current_condition',
+         'income_source:ntext',
+         'income_level',
+         'construction_type:ntext',
+         'current_income_status',
+         'damage_type',
+         'user_id',
+         'no_of_occupants',
+         'event_name',
+         'timestamp_created_at',
+         'timestamp_updated_at',
+         'timestamp_occurance',
+         'longitude',
+         'latitude',
+         'geom',
+         'wkt:ntext',
+         'address',
+         'c_code',
+         'z_code',
+         'd_code',
+         'v_code',
+         'ward_no',
+         'impact_death',
+         'impact_injured',
+         'impact_missing',
+         'impact_displaced',
+         'impact_orphaned',
+        // 'tags',
+    ];
+    ?>
+    <?php
+
+    // Renders a export dropdown menu
+    echo ExportMenu::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'emptyText'=>'Empty Result',
+        'target'=>ExportMenu::TARGET_SELF,
+    ]);
+    ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -59,7 +109,7 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'tags',
 
             ['class' => 'yii\grid\ActionColumn'],
-        ],
+        ]
     ]); ?>
 
 </div>
